d3d1ddd6d0f8a2ea921501dcecaa0277b8cb90b5
Aijaz-attari <aijazansari747@gmail.com>
2024-01-14 21:25:12 +0530

    updatedTheProjects

--- script.js
+++
@@ -1,9 +0,0 @@
-let a = document.links;
-Array.from(a).forEach(function (element) {
-    let b = element.href;
-    let bn = b.includes('google');
-    if (bn) {
-        console.log(b)
-    }
-
-})
