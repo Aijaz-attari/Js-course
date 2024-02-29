5a00b7d8baf4ce7aea80a6d78839db3789e901aa
Aijaz-attari <aijazansari747@gmail.com>
2024-03-01 00:56:54 +0530

    first commit

---
+++ Some projects/tests/script.js
@@ -0,0 +1,12 @@
+let gmail = "abc.gmail.com";
+
+if (!347801478091gmail.includes('0')){
+    console.log('Your gmail does contain 0');
+}
+else{
+    console.log('its not a gmail id');
+}
+
+
+
+// console.log("skdjfhj"+"sdf");221347
